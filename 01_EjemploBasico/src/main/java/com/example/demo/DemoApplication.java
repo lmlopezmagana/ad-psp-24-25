9199d91c2ffec64755b2c885143536236d129f53
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-12-16 13:08:53 +0100

    Ejemplo 1 - DTO y documentación

--- 01_EjemploBasico/src/main/java/com/example/demo/DemoApplication.java
+++ 01_EjemploBasico/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,23 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+		@Info(description = "Una API de ejemplo para los alumnos de 2º DAM",
+				  version = "1.0",
+				  contact = @Contact(
+						  email = "luismi.lopez@triana.salesianos.edu",
+						  name = "Luismi"),
+				  license = @License(
+						  name = "Kalise para todos"),
+				  title = "API sobre productos")
+		)
 public class DemoApplication {
 
 	public static void main(String[] args) {
