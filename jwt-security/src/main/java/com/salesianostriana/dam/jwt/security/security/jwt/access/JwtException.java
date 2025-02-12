045ac6c9fa22dd47f81f0f3718265c4e827b5a0f
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2025-02-12 14:27:39 +0100

    Seguridad sin gestión de errores

---
+++ jwt-security/src/main/java/com/salesianostriana/dam/jwt/security/security/jwt/access/JwtException.java
@@ -0,0 +1,7 @@
+package com.salesianostriana.dam.jwt.security.security.jwt.access;
+
+public class JwtException extends RuntimeException {
+    public JwtException(String message) {
+        super(message);
+    }
+}
