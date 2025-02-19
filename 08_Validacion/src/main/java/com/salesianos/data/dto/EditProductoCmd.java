f7541fa0364bf6fc749550f3374412bb7ae51ab8
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2025-02-19 13:57:04 +0100

    i18n de la validación

--- 08_Validacion/src/main/java/com/salesianos/data/dto/EditProductoCmd.java
+++ 08_Validacion/src/main/java/com/salesianos/data/dto/EditProductoCmd.java
@@ -5,12 +5,12 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 
 public record EditProductoCmd(
-        @NotBlank
+        @NotBlank(message = "{editProductoCmd.nombre.notblank}")
         String nombre,
         @NotBlank
         String descripcion,
         @DecimalMin("0.01")
-        @DecimalMax("99.99")
+        @DecimalMax(value = "99.99", message = "Simio no mata simio")
         double precio,
         Long categoriaId
 ) {
