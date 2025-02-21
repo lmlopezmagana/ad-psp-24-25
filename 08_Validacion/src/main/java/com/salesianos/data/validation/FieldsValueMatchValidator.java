5b2d922b348f70caa00a55c79dd667594bda2886
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2025-02-21 14:20:57 +0100

    Validadores personalizados. Minor bugs fixed

--- 08_Validacion/src/main/java/com/salesianos/data/validation/FieldsValueMatchValidator.java
+++ 08_Validacion/src/main/java/com/salesianos/data/validation/FieldsValueMatchValidator.java
@@ -23,7 +23,7 @@ public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValu
                 .forBeanPropertyAccess(o).getPropertyValue(fieldMatch);
 
         if (fieldValue != null)
-            return fieldMatch.equals(fieldValueMatch);
+            return fieldValue.equals(fieldValueMatch);
         else
             return fieldValueMatch == null;
 
